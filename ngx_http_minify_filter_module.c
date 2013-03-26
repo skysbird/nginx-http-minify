4115d4dca592affe5e4ce93ceadb4ee3e7f8caa4
skysbird <skysbird@gmail.com>
2013-03-27 01:43:03 +0800

    added jsmin module and finished the base functional

--- ngx_http_minify_filter_module.c
+++ ngx_http_minify_filter_module.c
@@ -1,6 +1,7 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
+#include "ngx_jsmin.h"
 
 
 
@@ -78,6 +79,8 @@ ngx_module_t  ngx_http_minify_filter_module = {
 static ngx_http_output_header_filter_pt  ngx_http_next_header_filter;
 static ngx_http_output_body_filter_pt    ngx_http_next_body_filter;
 
+static ngx_int_t ngx_http_minify_buf(ngx_buf_t *buf,ngx_http_request_t *r,ngx_open_file_info_t *of);
+
 static ngx_int_t
 ngx_http_minify_header_filter(ngx_http_request_t *r)
 {
@@ -96,8 +99,7 @@ ngx_http_minify_header_filter(ngx_http_request_t *r)
     {
         return ngx_http_next_header_filter(r);
     }
-
-
+    ngx_http_clear_content_length(r);
     return ngx_http_next_header_filter(r);
 }
 
@@ -106,21 +108,91 @@ static ngx_int_t
 ngx_http_minify_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
 
-//    ngx_buf_t             *b;
-    ngx_chain_t           *cl;
-    ngx_http_minify_conf_t  *conf;
-  
-    conf = ngx_http_get_module_loc_conf(r,ngx_http_minify_filter_module);
+    ngx_buf_t             	*b;
+    ngx_chain_t           	*cl;
+    ngx_open_file_info_t         of;
+    ngx_http_core_loc_conf_t    *ccf;
+    ngx_http_minify_conf_t      *conf;
+    ngx_str_t                   *filename;
+    ngx_uint_t                  level;
+    ngx_int_t                   rc;
 
-    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "http minify filter");
+    ccf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
+    conf = ngx_http_get_module_loc_conf(r,ngx_http_minify_filter_module);
+    if (!conf->enable){
+        return ngx_http_next_body_filter(r,in);
+    }
+
     if(ngx_http_test_content_type(r, &conf->types) == NULL){
         return ngx_http_next_body_filter(r,in);
     }
 
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                   "http minify filter");
     cl = in; 
     while (cl) {
+        ngx_memzero(&of, sizeof(ngx_open_file_info_t));
+
+        of.read_ahead = ccf->read_ahead;
+        of.directio = ccf->directio;
+        of.valid = ccf->open_file_cache_valid;
+        of.min_uses = ccf->open_file_cache_min_uses;
+        of.errors = ccf->open_file_cache_errors;
+        of.events = ccf->open_file_cache_events;
+
+ 	b = cl->buf;
+	filename = &cl->buf->file->name;
+	if (ngx_open_cached_file(ccf->open_file_cache, filename, &of, r->pool)
+            != NGX_OK)
+        {
+            switch (of.err) {
+
+            case 0:
+                return NGX_HTTP_INTERNAL_SERVER_ERROR;
+
+            case NGX_ENOENT:
+            case NGX_ENOTDIR:
+            case NGX_ENAMETOOLONG:
+
+                level = NGX_LOG_ERR;
+                rc = NGX_HTTP_NOT_FOUND;
+                break;
+
+            case NGX_EACCES:
+
+                level = NGX_LOG_ERR;
+                rc = NGX_HTTP_FORBIDDEN;
+                break;
+
+            default:
+
+                level = NGX_LOG_CRIT;
+                rc = NGX_HTTP_INTERNAL_SERVER_ERROR;
+                break;
+            }
+
+            if (rc != NGX_HTTP_NOT_FOUND || ccf->log_not_found) {
+                ngx_log_error(level, r->connection->log, of.err,
+                              "%s \"%V\" failed", of.failed, filename);
+            }
+
+
+            return rc;
+        }
+
+	if (!of.is_file) {
+	    ngx_log_error(NGX_LOG_CRIT, r->connection->log, ngx_errno,
+	    	      "\"%V\" is not a regular file", filename);
+	    return NGX_HTTP_NOT_FOUND;
+	}
+
+	if (of.size == 0) {
+	    continue;
+	}
+
+ 	ngx_http_minify_buf(b,r,&of);	
         cl = cl->next;
+
     }
 
     return ngx_http_next_body_filter(r,in);
@@ -128,7 +200,90 @@ ngx_http_minify_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 }
 
 
+static ngx_int_t 
+ngx_http_minify_buf(ngx_buf_t *buf,ngx_http_request_t *r,ngx_open_file_info_t *of)
+{
+    ngx_file_t *src_file ;
+    ngx_buf_t *b;
+    ngx_buf_t *dst = NULL;
+    ngx_buf_t *min_dst = NULL;
+    ngx_int_t size;
 
+    src_file =  ngx_pcalloc(r->pool, sizeof(ngx_file_t));
+    if (src_file == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    src_file->fd = of->fd;
+    src_file->name = buf->file->name;
+    src_file->log = r->connection->log;
+    src_file->directio = of->is_directio;
+
+    size = of->size;
+    b = ngx_calloc_buf(r->pool); 
+    if (b == NULL) {
+        //return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    b->start = ngx_palloc(r->pool, size+1);
+    b->pos = b->start;
+    b->last = b->start;
+    b->end = b->last + size ;
+    b->temporary = 1;
+
+    dst = b;
+
+    #if (NGX_HAVE_FILE_AIO)
+    
+         ngx_output_chain_ctx_t       *ctx;
+         ctx = ngx_http_get_module_ctx(r, ngx_http_minify_filter_module);
+         if (ctx->aio_handler) {
+             n = ngx_file_aio_read(src_file, dst->pos, (size_t) size,
+        			   0, ctx->pool);
+             if (n == NGX_AGAIN) {
+        	 ctx->aio_handler(ctx, src->file);
+        	 return NGX_AGAIN;
+             }
+    
+         } else {
+             n = ngx_read_file(src_file, dst->pos, (size_t) size,
+        		       0);
+         }
+    #else
+    
+        ngx_read_file(src_file, dst->pos, (size_t) size, 0);
+    #endif
+        dst->end[0] = 0;
+    
+        b = ngx_calloc_buf(r->pool); 
+        if (b == NULL) {
+            //return NGX_HTTP_INTERNAL_SERVER_ERROR;
+        }
+    
+        b->start = ngx_palloc(r->pool, size);
+        b->pos = b->start;
+        b->last = b->start;
+        b->end = b->last + size;
+        b->temporary = 1;
+    
+        min_dst = b;
+        jsmin(dst,min_dst);
+
+        buf->start = min_dst->start;     
+        buf->pos = min_dst->pos;
+        buf->last = min_dst->last;
+        buf->end = min_dst->end;
+        buf->memory = 1;
+        buf->in_file = 0;
+
+//        ngx_memzero(&of, sizeof(ngx_open_file_info_t));
+
+
+    
+
+    return NGX_OK;
+
+}
 static void *
 ngx_http_minify_create_conf(ngx_conf_t *cf)
 {
---
+++ ngx_jsmin.h
@@ -0,0 +1,3 @@
+void jsmin(ngx_buf_t *in,ngx_buf_t *out);
+
+
