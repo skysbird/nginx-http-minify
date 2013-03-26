a7c59999c8f2c0fc13324a11025bb39336924c1b
root <root@skysbird-System-Product-Name.(none)>
2013-03-26 18:12:07 +0800

    process the content_type of the filter and add the jsmin module

--- ngx_http_minify_filter_module.c
+++ ngx_http_minify_filter_module.c
@@ -1,15 +1,7 @@
-
-/*
- * Copyright (C) Igor Sysoev
- * Copyright (C) Nginx, Inc.
- */
-
-
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-#include <zlib.h>
 
 
 typedef struct {
@@ -45,6 +37,11 @@ static ngx_command_t  ngx_http_minify_filter_commands[] = {
 
       ngx_null_command
 };
+
+static ngx_int_t ngx_http_minify_filter_init(ngx_conf_t *cf);
+static void *ngx_http_minify_create_conf(ngx_conf_t *cf);
+static char *ngx_http_minify_merge_conf(ngx_conf_t *cf, void *parent, void *child);
+
 
 
 static ngx_http_module_t  ngx_http_minify_filter_module_ctx = {
@@ -84,8 +81,6 @@ static ngx_http_output_body_filter_pt    ngx_http_next_body_filter;
 static ngx_int_t
 ngx_http_minify_header_filter(ngx_http_request_t *r)
 {
-    ngx_table_elt_t       *h;
-    ngx_http_minify_ctx_t   *ctx;
     ngx_http_minify_conf_t  *conf;
 
     conf = ngx_http_get_module_loc_conf(r, ngx_http_minify_filter_module);
@@ -96,64 +91,12 @@ ngx_http_minify_header_filter(ngx_http_request_t *r)
             && r->headers_out.status != NGX_HTTP_NOT_FOUND)
         || (r->headers_out.content_encoding
             && r->headers_out.content_encoding->value.len)
-        || (r->headers_out.content_length_n != -1
-            && r->headers_out.content_length_n < conf->min_length)
         || ngx_http_test_content_type(r, &conf->types) == NULL
         || r->header_only)
     {
         return ngx_http_next_header_filter(r);
     }
 
-    r->minify_vary = 1;
-
-#if (NGX_HTTP_DEGRADATION)
-    {
-    ngx_http_core_loc_conf_t  *clcf;
-
-    clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
-
-    if (clcf->minify_disable_degradation && ngx_http_degraded(r)) {
-        return ngx_http_next_header_filter(r);
-    }
-    }
-#endif
-
-    if (!r->minify_tested) {
-        if (ngx_http_minify_ok(r) != NGX_OK) {
-            return ngx_http_next_header_filter(r);
-        }
-
-    } else if (!r->minify_ok) {
-        return ngx_http_next_header_filter(r);
-    }
-
-    ctx = ngx_pcalloc(r->pool, sizeof(ngx_http_minify_ctx_t));
-    if (ctx == NULL) {
-        return NGX_ERROR;
-    }
-
-    ngx_http_set_ctx(r, ctx, ngx_http_minify_filter_module);
-
-    ctx->request = r;
-    ctx->buffering = (conf->postpone_minifyping != 0);
-
-    ngx_http_minify_filter_memory(r, ctx);
-
-    h = ngx_list_push(&r->headers_out.headers);
-    if (h == NULL) {
-        return NGX_ERROR;
-    }
-
-    h->hash = 1;
-    ngx_str_set(&h->key, "Content-Encoding");
-    ngx_str_set(&h->value, "minify");
-    r->headers_out.content_encoding = h;
-
-    r->main_filter_need_in_memory = 1;
-
-    ngx_http_clear_content_length(r);
-    ngx_http_clear_accept_ranges(r);
-    ngx_http_clear_etag(r);
 
     return ngx_http_next_header_filter(r);
 }
@@ -162,167 +105,26 @@ ngx_http_minify_header_filter(ngx_http_request_t *r)
 static ngx_int_t
 ngx_http_minify_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
-    int                   rc;
-    ngx_chain_t          *cl;
-    ngx_http_minify_ctx_t  *ctx;
 
-    ctx = ngx_http_get_module_ctx(r, ngx_http_minify_filter_module);
-
-    if (ctx == NULL || ctx->done || r->header_only) {
-        return ngx_http_next_body_filter(r, in);
-    }
+//    ngx_buf_t             *b;
+    ngx_chain_t           *cl;
+    ngx_http_minify_conf_t  *conf;
+  
+    conf = ngx_http_get_module_loc_conf(r,ngx_http_minify_filter_module);
 
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
                    "http minify filter");
-
-    if (ctx->buffering) {
-
-        /*
-         * With default memory settings zlib starts to output minifyped data
-         * only after it has got about 90K, so it makes sense to allocate
-         * zlib memory (200-400K) only after we have enough data to compress.
-         * Although we copy buffers, nevertheless for not big responses
-         * this allows to allocate zlib memory, to compress and to output
-         * the response in one step using hot CPU cache.
-         */
-
-        if (in) {
-            switch (ngx_http_minify_filter_buffer(ctx, in)) {
-
-            case NGX_OK:
-                return NGX_OK;
-
-            case NGX_DONE:
-                in = NULL;
-                break;
-
-            default:  /* NGX_ERROR */
-                goto failed;
-            }
-
-        } else {
-            ctx->buffering = 0;
-        }
+    if(ngx_http_test_content_type(r, &conf->types) == NULL){
+        return ngx_http_next_body_filter(r,in);
     }
 
-    if (ctx->preallocated == NULL) {
-        if (ngx_http_minify_filter_deflate_start(r, ctx) != NGX_OK) {
-            goto failed;
-        }
+    cl = in; 
+    while (cl) {
+        cl = cl->next;
     }
 
-    if (in) {
-        if (ngx_chain_add_copy(r->pool, &ctx->in, in) != NGX_OK) {
-            goto failed;
-        }
-    }
+    return ngx_http_next_body_filter(r,in);
 
-    if (ctx->nomem) {
-
-        /* flush busy buffers */
-
-        if (ngx_http_next_body_filter(r, NULL) == NGX_ERROR) {
-            goto failed;
-        }
-
-        cl = NULL;
-
-        ngx_chain_update_chains(r->pool, &ctx->free, &ctx->busy, &cl,
-                                (ngx_buf_tag_t) &ngx_http_minify_filter_module);
-        ctx->nomem = 0;
-    }
-
-    for ( ;; ) {
-
-        /* cycle while we can write to a client */
-
-        for ( ;; ) {
-
-            /* cycle while there is data to feed zlib and ... */
-
-            rc = ngx_http_minify_filter_add_data(r, ctx);
-
-            if (rc == NGX_DECLINED) {
-                break;
-            }
-
-            if (rc == NGX_AGAIN) {
-                continue;
-            }
-
-
-            /* ... there are buffers to write zlib output */
-
-            rc = ngx_http_minify_filter_get_buf(r, ctx);
-
-            if (rc == NGX_DECLINED) {
-                break;
-            }
-
-            if (rc == NGX_ERROR) {
-                goto failed;
-            }
-
-
-            rc = ngx_http_minify_filter_deflate(r, ctx);
-
-            if (rc == NGX_OK) {
-                break;
-            }
-
-            if (rc == NGX_ERROR) {
-                goto failed;
-            }
-
-            /* rc == NGX_AGAIN */
-        }
-
-        if (ctx->out == NULL) {
-            ngx_http_minify_filter_free_copy_buf(r, ctx);
-
-            return ctx->busy ? NGX_AGAIN : NGX_OK;
-        }
-
-        if (!ctx->gzheader) {
-            if (ngx_http_minify_filter_gzheader(r, ctx) != NGX_OK) {
-                goto failed;
-            }
-        }
-
-        rc = ngx_http_next_body_filter(r, ctx->out);
-
-        if (rc == NGX_ERROR) {
-            goto failed;
-        }
-
-        ngx_http_minify_filter_free_copy_buf(r, ctx);
-
-        ngx_chain_update_chains(r->pool, &ctx->free, &ctx->busy, &ctx->out,
-                                (ngx_buf_tag_t) &ngx_http_minify_filter_module);
-        ctx->last_out = &ctx->out;
-
-        ctx->nomem = 0;
-
-        if (ctx->done) {
-            return rc;
-        }
-    }
-
-    /* unreachable */
-
-failed:
-
-    ctx->done = 1;
-
-    if (ctx->preallocated) {
-        deflateEnd(&ctx->zstream);
-
-        ngx_pfree(r->pool, ctx->preallocated);
-    }
-
-    ngx_http_minify_filter_free_copy_buf(r, ctx);
-
-    return NGX_ERROR;
 }
 
 
@@ -361,7 +163,7 @@ ngx_http_minify_merge_conf(ngx_conf_t *cf, void *parent, void *child)
 
     if (ngx_http_merge_types(cf, &conf->types_keys, &conf->types,
                              &prev->types_keys, &prev->types,
-                             ngx_http_html_default_types)
+                             ngx_http_minify_default_types)
         != NGX_OK)
     {
         return NGX_CONF_ERROR;
